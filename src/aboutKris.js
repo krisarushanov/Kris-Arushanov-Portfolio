4035a988fa2ce89b3eb1fd5917157ddba3b7d1ea
Kristena Arushanov <kris.arushanov@gmail.com>
2021-03-28 21:19:50 -0700

    added contact form button

--- src/aboutKris.js
+++
@@ -1,6 +0,0 @@
-let aboutKris = {
-    "imagebaseurl": "https://github.com/krisarushanov",
-    "name": "Kris Arushanov",
-    "role": "Full Stack Developer",
-    "linkedinId": "https://www.linkedin.com/in/kris-arushanov-39623a115/",
-   }
