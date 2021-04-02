8a945b984312d13e5ecd8c3593b8bc9cb903e633
Kristena Arushanov <kris.arushanov@gmail.com>
2021-04-02 02:12:03 -0700

    created form logic file

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.submit').click(function (event) {
+        event.preventDefault()
+        console.log('Clicked')
+
+        var email = $('.email').val()
+        var message = $('#message').val()
+
+       
+    })
+})
